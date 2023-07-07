bd135f140388e1d3662a4caa2be0fc4ab8cf15d7
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-07-07 23:43:35 +0530

    Add feature section

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ const menuBtn = document.querySelector(".menu-button");
 menuBtn.addEventListener("click", function () {
     document.querySelector(".site-logo").classList.toggle("alt-logo");
 })
+
+const featureLinks = [...document.querySelectorAll(".feature-link")];
+const featureImages = [...document.querySelectorAll(".feature-img")];
+const featureH1 = [...document.querySelectorAll(".feature-h1")];
+const featurePara = [...document.querySelectorAll(".feature-p")];
+
+
+featureLinks.forEach(function (link, index) {
+    link.addEventListener("click", function () {
+
+        featureImages.forEach(function (image) {
+            image.style.display = "none";
+        });
+
+        featureImages[index].style.display = "block";
+
+        featureH1.forEach(function (x) {
+            x.style.display = "none";
+        });
+
+        featureH1[index].style.display = "block";
+
+        featurePara.forEach(function (x) {
+            x.style.display = "none";
+        });
+
+        featurePara[index].style.display = "block";
+    });
+});
