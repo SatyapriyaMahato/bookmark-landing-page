ddfcb66ffb8cf54bd821fc640743c21d5c2411e3
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-07-09 14:52:39 +0530

    add faq section

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// menu section
 const menuBtn = document.querySelector(".menu-button");
 menuBtn.addEventListener("click", function () {
     document.querySelector(".site-logo").classList.toggle("alt-logo");
     document.body.classList.toggle("fixed-position");
 })
 
+// feature section
 const featureLinks = [...document.querySelectorAll(".feature-link")];
 const featureImages = [...document.querySelectorAll(".feature-img")];
 const featureH1 = [...document.querySelectorAll(".feature-h1")];
@@ -32,3 +34,32 @@ featureLinks.forEach(function (link, index) {
         featurePara[index].style.display = "block";
     });
 });
+
+
+// faq section
+
+const qs = [...document.querySelectorAll(".questions")];
+const arrowIcons = [...document.querySelectorAll(".faq-arrow")];
+
+qs.forEach(function (q, i) {
+    q.querySelector(".q-head").addEventListener("click", function () {
+        q.querySelector(".faq-arrow").classList.toggle("rotate");
+        let content = q.querySelector(".answer");
+        faqAnimation(content);
+    })
+})
+arrowIcons.forEach(function (a, i) {
+    a.addEventListener("click", function () {
+        a.classList.toggle("rotate");
+        let content = a.nextElementSibling.nextElementSibling;
+        faqAnimation(content);
+    })
+})
+
+function faqAnimation(content) {
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
